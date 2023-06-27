03deb6bde01e8042753afb5bca9de750f5ac24ab
ever-19 <everrevollo07@gmail.com>
2023-06-27 09:23:42 -0400

    actualizando footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,10 +4,10 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="inner-content">
-                        <p>Copyright &copy; 2020 Sixteen Clothing Co., Ltd.
+                        <p>Copyright &copy; 2023 Proyecto Ferreteria.
 
-                            - Design: <a rel="nofollow noopener" href="https://templatemo.com"
-                                target="_blank">TemplateMo</a></p>
+                            - SIS257 : INFORMÁTICA - <a rel="nofollow noopener" href="https://usfx.bo/#gsc.tab=0"
+                                target="_blank">U.M.R.P.S.F.X.CH.</a></p>
                     </div>
                 </div>
             </div>
